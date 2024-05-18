a0a1d8072605a7a190552fec62d283c231c786a5
RFFRNDA <rafiffernanda123@gmail.com>
2024-05-18 23:11:26 +0700

    fix : Page Beranda

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,8 +1,14 @@
 <nav>
     <img class="logo" src="images/img/logo.png" alt="Logo">
     <ul class="navbar-links">
-        <li><a href="#">Beranda</a></li>
-        <li><a href="#">Menu</a></li>
-        <li><a href="#">Cek Pesanan</a></li>
+        <li>
+            <a class="{{ ($active === "beranda") ? 'active' : '' }}" href="/">Beranda</a>
+        </li>
+        <li>
+            <a href="#">Menu</a>
+        </li>
+        <li>
+            <a href="#">Cek Pesanan</a>
+        </li>
     </ul>
 </nav>
